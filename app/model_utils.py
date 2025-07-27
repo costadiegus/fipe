5a5fe405071581434700a1f009f1218a37004ded
DG Costa <costadiegus@gmail.com>
2025-07-27 18:58:58 -0300

    Add scaling to training data and prepare for RandomForestRegressor

--- app/model_utils.py
+++ app/model_utils.py
@@ -3,6 +3,7 @@ import pickle
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import warnings
 
@@ -81,9 +82,21 @@ class CarPriceModel:
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2, random_state=42
         )
+        # ESCALONAMENTO
+        # Escalamos os dados para otimização do treinamento do modelo
+        from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+        # scaler = StandardScaler()
+        scaler = MinMaxScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+        X_train, X_test = X_train_scaled, X_test_scaled
 
         # Treinar modelo
         self.model = DecisionTreeRegressor(random_state=42)
+        # self.model = RandomForestRegressor(
+        #    random_state=42, max_features="sqrt", n_estimators=30
+        # )
         self.model.fit(X_train, y_train)
 
         # Avaliar modelo
